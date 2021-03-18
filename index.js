ce8e21c6f1a84822f49510a28a789e61e0de586a
TylerL <tluckewicz@gmail.com>
2021-03-18 14:08:59 -0400

    Finsished assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let newArray = [];
+    let message;
+    for (let i = 0; i < array.length; i++) {
+        message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        newArray.push(message);
+    }
+
+    return newArray;
+}
+
+console.log(writeCards(['tim', 'dan', 'bill'], 'christmas'));
+
+
+function countDown(integer) {
+    while (integer > -1) {
+        console.log(integer);
+        integer--;
+    }
+}
+
